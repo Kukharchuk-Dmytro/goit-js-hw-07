f54ea2a4c20bd1dca2e791d5cb736139a01089b0
Kukharchuk-Dmytro <rabendp@gmail.com>
2020-09-20 22:52:58 +0300

    1

---
+++ js/task-1.js
@@ -0,0 +1,4 @@
+const x = document.querySelector("h2");
+console.log(`категория`, x.textContent);
+const y = document.querySelector(".ul");
+console.log(`Количество елементов`, y.childElementCount);
